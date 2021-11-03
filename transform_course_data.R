8d7bdeedfd0c9df07a40bc39b4208363a2de0490
Angel Sarmiento <angelsarmiento409@gmail.com>
2021-11-02 20:04:36 -0400

    Added some topic modeling investigation

--- transform_course_data.R
+++ transform_course_data.R
@@ -1,3 +1,15 @@
+
+clean_columns <- function(data) {
+  colnames(data) <- str_replace_all(colnames(data), " |\\/", "_")
+  colnames(data) <- str_replace_all(colnames(data), "\\:|\\?|\\_\\(Rendered_no_HTML\\)|\\(s\\)", "")
+  
+  # Testing to see whether or not uniting course code and number creates a unique enough column for joining
+  data_new_code <- data %>% unite("Course_ID", Prefix:Code, sep = "_", remove = TRUE) %>% 
+    distinct(Course_ID, .keep_all = TRUE)
+  return(data_new_code)
+}
+
+
 # This is a very simple function that solves a problem with course data received at Florida Poly. Not super generalizable but can be made to 
 # be so at some point. 
 
@@ -6,14 +18,7 @@ transform_course_data <- function(data, target_column = "Course_Outline", filter
   require(dplyr)
   require(tidyr)
   require(stringr)
-  
-  
-  colnames(data) <- str_replace_all(colnames(data), " |\\/", "_")
-  colnames(data) <- str_replace_all(colnames(data), "\\:|\\?|\\_\\(Rendered_no_HTML\\)|\\(s\\)", "")
-  
-  # Testing to see whether or not uniting course code and number creates a unique enough column for joining
-  data_new_code <- data %>% unite("Course_ID", Prefix:Code, sep = "_", remove = TRUE) %>% 
-    distinct(Course_ID, .keep_all = TRUE)
+ 
   
   # Bit of extra regex to filter down the data to the levels that we need
   filtering_string <- regex(paste0("^Week|\\s{2}|^\\s{1}|^Quiz|^Chapter|^Case|^http|Ch.|^Incoterms|Exam|Presentations|www|", 
@@ -25,26 +30,26 @@ transform_course_data <- function(data, target_column = "Course_Outline", filter
   
   
   # This will eventually be its own script
-  outl_df <- data_new_code %>% 
-    mutate(Outlines = strsplit(as.character(Course_Outline), "[\\\r\\\n\\\t]+")) 
+  outl_df <- data %>% 
+    mutate(new_col = strsplit(as.character(Course_Description), "[\\\r\\\n\\\t]+")) 
   
-  main_outl_df <- outl_df %>% tidyr::separate_rows(Outlines, sep = "^[0-9].")  %>% filter(grepl("^\\d{1}\\. |\\d{2}\\. ", Outlines))
+  main_outl_df <- outl_df %>% tidyr::separate_rows(new_col, sep = "^[0-9].")  %>% filter(!grepl("^\\d{1}\\. |\\d{2}\\. ", new_col))
   # Getting everything else just in case we need them
-  side_outl_df <- outl_df %>% tidyr::separate_rows(Outlines, sep = "^[0-9].") %>% filter(!grepl(paste0("^\\d{1}\\. |\\d{2}\\. "), Outlines)) 
+  side_outl_df <- outl_df %>% tidyr::separate_rows(new_col, sep = "^[0-9].") %>% filter(!grepl(paste0("^\\d{1}\\. |\\d{2}\\. "), new_col)) 
   
-  main_outl_df$Outlines <- main_outl_df$Outlines %>% 
+  main_outl_df$new_col <- main_outl_df$new_col %>% 
     str_replace_all(replace_string, "")
   
   
   # Separate 
   side_outl_new <- side_outl_df %>% 
     filter(!grepl(filtering_string, 
-                  Outlines, ignore.case = TRUE)) %>% 
-    filter(!is.na(Outlines))
+                  new_col, ignore.case = TRUE)) %>% 
+    filter(!is.na(new_col))
   
   
-  side_outl_new$Outlines <- side_outl_new$Outlines %>% str_replace_all(replace_string, "")
-  # Joining the two dataframes for the outlines
+  side_outl_new$new_col <- side_outl_new$new_col %>% str_replace_all(replace_string, "")
+  # Joining the two dataframes for the new_col
   full_outl <- main_outl_df %>% rbind(side_outl_new)
   write.csv(full_outl, here("data/full_outline_data.csv"), row.names = FALSE)
   return(full_outl)
